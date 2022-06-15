fc1af27f4ec70b4c4dea5744bb3c5836c4686cb8
iAvearges <31937542+iAverages@users.noreply.github.com>
2022-06-15 22:34:19 +0100

    Example items

--- src/items.ts
+++ src/items.ts
@@ -1,8 +1,32 @@
+import { type } from "os";
 import { IItem } from "./Item";
 
 type ItemCategories = Record<string, IItem[]>;
 export const items: ItemCategories = {
-    category_name: [{ image: "Link to image", name: "name", url: "Link" }],
+    examples: [
+        {
+            name: "Example Item 1",
+            image: "https://cdn.danielraybone.com/assets/local/portal/prometheus.png",
+            url: "http://local.danielraybone.com/#",
+        },
+        {
+            name: "Example Item 2",
+            image: "https://cdn.danielraybone.com/assets/local/portal/grafana.png",
+            url: "http://local.danielraybone.com/#",
+        },
+    ],
+    another: [
+        {
+            name: "Example Item 3",
+            image: "https://cdn.danielraybone.com/assets/local/portal/pihole.png",
+            url: "http://local.danielraybone.com/#",
+        },
+        {
+            name: "Example Item 4",
+            image: "https://cdn.danielraybone.com/assets/local/portal/router.png",
+            url: "http://local.danielraybone.com/#",
+        },
+    ],
 };
 
 export default items;
